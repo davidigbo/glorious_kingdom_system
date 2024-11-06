728a4004b6cba1a106fe8166a57ecae7ede9652e
david30-maker <davidokpah396@gmail.com>
2024-11-06 16:09:07 +0100

    create  post controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
-end
+    before_action :set_post, only: %i[show edit update destroy]
+
+    def index
+      @posts = Post.order(published_at: :desc)
+    end
+
+    def show
+    end
+
+    def new
+      @post = Post.new
+    end
+
+    def create
+      @post = Post.new(post_params)
+      if @post.after_save
+        redirect_to @post, notice: 'Post was successfully created'
+      else
+        render new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @post.updste(podt_params)
+        redirect_to @post, notice: 'Post aws successfully updated'
+      else
+        render :edit
+      end
+    end
+
+end 
