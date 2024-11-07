fffaa50f1b48d42d3a0d1321b94909fff61328f2
david30-maker <davidokpah396@gmail.com>
2024-11-07 22:44:14 +0100

    modify new and edit view post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+    
+    scope :published, -> { where('published_at <= ?', Time.now) }
+    scope :draft, -> { where('published_at > ?', Time.now) }
+    
+    def published?
+        published_at <= Time.now
+    end
 end
