d93d73280acce247959ee3b4d3d1bcd0c7be625e
david30-maker <davidokpah396@gmail.com>
2024-11-07 23:38:22 +0100

    create comment controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
+
+    belongs_to :user
+    has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
+    
     validates :title, presence: true
     validates :content, presence: true
     
